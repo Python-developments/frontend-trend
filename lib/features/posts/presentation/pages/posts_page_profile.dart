6f783f954e857802cae22475e0a5ea05ead20864
shady <shady.samara@gmail.com>
2024-12-07 10:38:49 +0200

    complete profile posts

--- lib/features/posts/presentation/pages/posts_page_profile.dart
+++ lib/features/posts/presentation/pages/posts_page_profile.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_list_view/flutter_list_view.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:frontend_trend/features/posts/presentation/bloc/posts_bloc/profile_posts_bloc.dart';
 import 'package:frontend_trend/features/profile/presentation/bloc/current_user_cubit/current_user_cubit.dart';
@@ -35,68 +36,15 @@ class PostsUserPage extends StatefulWidget {
 }
 
 class _PostsPageState extends State<PostsUserPage> {
-  late PaginationParam _paginationParams;
-  late ScrollController _scrollController;
-  List<GlobalKey> _itemKeys = [];
   int page = 1;
-
+  FlutterListViewController controller = FlutterListViewController();
   @override
   void initState() {
     super.initState();
-    _paginationParams = PaginationParam(page: 1);
+    controller.sliverController.jumpToIndex(widget.index);
     context.read<ProfilePostsBloc>().add(InitialocalReactionEvent(
         posts: widget.posts,
         user: context.read<CurrentUserCubit>().state.user));
-
-    _scrollController = ScrollController();
-
-    // Dynamically generate a unique GlobalKey for each post
-    _itemKeys = List.generate(widget.posts.length, (_) => GlobalKey());
-
-    // Ensure scrolling happens after layout
-  }
-
-  void _getAllPosts() {
-    context
-        .read<PostsBloc>()
-        .add(GetAllPostsEvent(params: (_paginationParams..page = 1)));
-  }
-
-  void _scrollToInitialIndex() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (widget.index >= 0 && widget.index < widget.posts.length) {
-        final key = _itemKeys[widget.index];
-        final renderBox = key.currentContext?.findRenderObject() as RenderBox?;
-        log("widget position is ${key.currentContext}");
-        if (renderBox != null) {
-          // Calculate the position of the item relative to the screen
-          final position = renderBox.localToGlobal(Offset.zero).dy;
-          log("widget position is $position");
-          // Dynamically calculate the AppBar height
-          final appBarHeight = AppBar().preferredSize.height;
-
-          // Account for the safe area (top padding for devices with notches or system UI elements)
-          final safeAreaTop = MediaQuery.of(context).padding.top;
-
-          // Optional: Add screen padding if needed
-          final screenPadding = 13.0.sp; // Adjust this as per your design
-
-          // Adjust the scroll position to ensure the entire item is visible
-          final adjustedPosition =
-              position - appBarHeight - safeAreaTop - screenPadding;
-
-          // Ensure the scroll position is valid (not negative)
-
-          if (adjustedPosition >= 0 &&
-              widget.index != widget.posts.length - 1) {
-            _scrollController.jumpTo(adjustedPosition);
-          } else if (widget.index == widget.posts.length - 1) {
-            _scrollController
-                .jumpTo(_scrollController.position.maxScrollExtent);
-          }
-        }
-      }
-    });
   }
 
   @override
@@ -125,35 +73,24 @@ class _PostsPageState extends State<PostsUserPage> {
               ToastUtils(context).showNoInternetConnectionToast();
             } else if (state is PostsErrorState) {
               ToastUtils(context).showCustomToast(message: state.message);
-            } else if (state is ProfilePostsLoadedState) {
-              _scrollToInitialIndex();
             }
           },
           builder: (context, state) {
             if (state is ProfilePostsLoadedState) {
-              List<PostModel> posts = state.posts;
-
-              return SingleChildScrollView(
-                controller: _scrollController,
-                child: Column(
-                    children: List.generate(posts.length, (index) {
-                  return Container(
-                    key: _itemKeys[index], // Unique key for each item
-                    child: SinglePostWidget(
-                      isprofilePost: true,
-                      profilePosts: widget.posts,
-                      key: _itemKeys[index], // Unique key for each item
-                      post: posts[index],
-                      function: () {},
-                    ),
-                  );
-                })),
-              );
+              List<PostModel> posts = widget.posts;
+              return ScrollablePositionedList.builder(
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) => SinglePostWidget(
+                        isprofilePost: true,
+                        profilePosts: widget.posts,
+                        post: posts[index],
+                        function: () {},
+                      ),
+                  initialScrollIndex: widget.index);
             } else if (state is PostsErrorState) {
-              return MyErrorWidget(
-                  message: state.message, onRetry: _getAllPosts);
+              return MyErrorWidget(message: state.message, onRetry: () {});
             } else if (state is PostsNoInternetConnectionState) {
-              return NoInternetConnectionWidget(onRetry: _getAllPosts);
+              return NoInternetConnectionWidget(onRetry: () {});
             }
 
             // Default shimmer state
